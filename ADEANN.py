a3a24eda3608cc235abae31f56fbc36222d0ac7d
j.almeida <teste@totvs.com.br>
2019-02-05 14:22:18 -0300

    add nova função imprime_genbin

--- ADEANN.py
+++ ADEANN.py
@@ -1,10 +1,10 @@
 import numpy as np
-print(np.random.random_integers(0,1))
+print(np.random.random_integers(0, 1))
 
 # arr = np.zeros(9)
 # arr.shape = (3,3)
 # print(arr)
-# from keras.models import Sequential 
+# from keras.models import Sequential
 # from keras.layers import Dense
 
 # model = Sequential()
@@ -17,10 +17,10 @@ print(np.random.random_integers(0,1))
 
 # # model.fit(x_train, y_train, epochs=5, batch_size=32)
 # ALGORITMO GENÉTICO
-GERACAO    = 1
+GERACAO = 1
 INDIVIDUOS = 60
-GENE       = 516
-NINTER     = 10000
+GENE = 516
+NINTER = 10000
 
 # REDE NEURAL
 TIPO = 4
@@ -41,7 +41,7 @@ INDIC = 0
 SUP = 0
 NEURIT = 0
 
-# TAXAS 
+# TAXAS
 TX_TRANS = 0.001
 ERR = 0.0
 EMQ = 0.0
@@ -50,22 +50,25 @@ AUX = 0
 SOMA_NINT_TOTAL = 0
 
 # CONSTANTES
-FIT = [[]]
-HIST_FIT = [[]]
+FIT = 0
+HIST_FIT = 0
 
-tab_converte = ['f','F','n','.','n','.','f','F','F','f','B','f','[','n','[','.',
-			'f',']','n','*','.','F','f','F',']','.','[','f','f','*','B',']',
-			'.',']','n','F','f','B','f','B','F','[','B','n','*','f','.',']',
-			']','[','n','F','n','B','[','.','f',']','B','F','B','f','*','[']
-    
+tab_converte = ['f', 'F', 'n', '.', 'n', '.', 'f', 'F', 'F', 'f', 'B', 'f', '[', 'n', '[', '.',
+                'f', ']', 'n', '*', '.', 'F', 'f', 'F', ']', '.', '[', 'f', 'f', '*', 'B', ']',
+                '.', ']', 'n', 'F', 'f', 'B', 'f', 'B', 'F', '[', 'B', 'n', '*', 'f', '.', ']',
+                ']', '[', 'n', 'F', 'n', 'B', '[', '.', 'f', ']', 'B', 'F', 'B', 'f', '*', '[']
+
+
 def zerar_fitness(fit):
     i, j = 0, 0
     fit = np.zeros(INDIVIDUOS * GENE)
-    fit.shape = (INDIVIDUOS , GENE)
+    fit.shape = (INDIVIDUOS, GENE)
+    return fit
+
 
 def imprime_hist_fitness(hist_fit, file, contador):
     # imprimir no txt
-    print("\n", contador) 
+    print("\n", contador)
     print("\t\t", hist_fit[contador-1][0])
     print("\t\t", hist_fit[contador-1][1])
 
@@ -76,5 +79,27 @@ def genotipo_estatico(individuos, gene):
     gen.shape = (individuos, gene)
     for i in range(0, gene):
         for j in range(0, individuos):
-            gen[i][j]= np.random.random_integers(0,1)
+            gen[i][j] = np.random.random_integers(0, 1)
+    return gen
+
+
+def gen_bin_dec_genotipo_dinamico(individuos, gene_dec):
+    gen_dec = np.zeros(individuos * gene_dec)
+    gen_dec.shape = (individuos, gene_dec)
+    return gen_dec
+
+
+def genotipo_dinamico_string(individuos, gene_dec):
+    gen_string = np.zeros(individuos * (gene_dec+1))
+    return (gen_string)
+
+
+def imprime_genbin(gen, individuos, gene, file):
+    i, j = 0, 0
+    print("\n")
+    for i in range(0, individuos):
+        for j in range(0, gene):
+            # fprintf(pFile,"encerrado")
+            print(gen[i][j])
+    return gen
 
